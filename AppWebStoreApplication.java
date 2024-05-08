d957a9192614126a1b35a38958d1c0d7b4f70c6f
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-08 00:58:53 -0500

    Creacion de la estructura del proyecto y las clases model

---
+++ AppWebStoreApplication.java
@@ -0,0 +1,13 @@
+package sena.appwebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebStoreApplication.class, args);
+	}
+
+}
